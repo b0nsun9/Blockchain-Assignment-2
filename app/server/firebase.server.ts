c7ed6702867d3c567d7e8bd8779137ebcdd58562
Bonsung Koo <developer@bonsung.me>
2025-05-05 15:03:20 +1200

    change server auth setting

--- app/server/firebase.server.ts
+++ app/server/firebase.server.ts
@@ -2,8 +2,6 @@ import { initializeApp, getApps, cert, getApp } from "firebase-admin/app"
 import type { App } from "firebase-admin/app"
 import { Auth, getAuth } from "firebase-admin/auth"
 
-import serviceKey from "./firebase-adminsdk.json"
-
 let app: App
 let auth: Auth
 
@@ -11,9 +9,9 @@ if (getApps().length === 0) {
   app = initializeApp({
     credential: cert(
       {
-        projectId: serviceKey.project_id,
-        clientEmail: serviceKey.client_email,
-        privateKey: serviceKey.private_key
+        projectId: process.env.SERVER_PROJECT_ID,
+        clientEmail: process.env.SERVER_CLIENT_EMAIL,
+        privateKey: process.env.SERVER_PRIVATE_KEY
       }
     )
   })
